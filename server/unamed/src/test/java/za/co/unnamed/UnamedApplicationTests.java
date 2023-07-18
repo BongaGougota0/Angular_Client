cc3fc2ab265b6a9ffbf7dcf52be07b8425b65972
Bonga <bonga_gougota@outlook.com>
2023-07-18 14:31:09 +0200

    server

---
+++ server/unamed/src/test/java/za/co/unnamed/UnamedApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.unnamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnamedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
