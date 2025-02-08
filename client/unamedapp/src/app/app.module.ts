a71430d628ab83796bfc8674676be03cda966037
BongaGougota0 <bonga_gougota@outlook.com>
2025-02-08 15:36:25 +0200

    back working on Angular

--- client/unamedapp/src/app/app.module.ts
+++ client/unamedapp/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
 @NgModule({
@@ -15,6 +16,9 @@ import { SignupComponent } from './signup/signup.component';
     HomeComponent,
     LoginComponent,
     SignupComponent,
+    FooterComponent,
+    HeaderComponent,
+    SideMenuComponent,
   ],
   imports: [
     BrowserModule,
